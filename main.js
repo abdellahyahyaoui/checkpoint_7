47354eeb45e6e08a9b7675a7a9fcede35af0ba72
free palestine <mariamsakina2010@gmail.com>
2024-05-22 12:49:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function checkPoint7(a, b, c, d) {
+  let suma1 = a + b;
+  let suma2 = c + d;
+  let resultado = suma1 * suma2;
+
+  if (resultado > 50) {
+    console.log("¡El número es mayor que 50!");
+  } else {
+    console.log("¡El número es menor que 50!");
+  }
+}
+
+checkPoint7(1, 0, 3, 4);
